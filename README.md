d65f6e77e7661de2964535eaa517505d822226ac
Samu Lang <langsamu@hotmail.com>
2019-05-14 16:59:25 +0100

    Implementation report generator in dnr now works

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ﻿This projects generates an [EARL](https://www.w3.org/TR/EARL10-Schema/) report for the [dotNetRDF SHACL Processor](https://github.com/dotnetrdf/dotnetrdf/pull/236) for submission to the [SHACL TestSuite and Implementation Report](https://w3c.github.io/data-shapes/data-shapes-test-suite/).
+
+**The latest report is [dotnetrdf-shacl-earl.ttl](dotnetrdf-shacl-earl.ttl).**
--- ReportGenerator/Program.cs
+++ ReportGenerator/Program.cs
@@ -1,65 +1,14 @@
 ﻿namespace ReportGenerator
 {
-    using System;
-    using System.Linq;
     using VDS.RDF;
     using VDS.RDF.Shacl;
-    using VDS.RDF.Shacl.Validation;
+    using VDS.RDF.Writing;
 
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var i = 1;
-
-            foreach (var item in TestSuiteData.CoreFullTests.Concat(TestSuiteData.SparqlTests).Select(x => (string)x[0]).ToList())
-            {
-                Console.Write("{0} ", i++);
-
-                TestSuiteData.ExtractTestData(item, out var testGraph, out var shouldFail, out var dataGraph, out var shapesGraph);
-
-                bool conforms()
-                {
-                    var report = new ShapesGraph(shapesGraph).Validate(dataGraph);
-
-                    var actual = report.Normalised;
-                    var expected = Report.Parse(testGraph).Normalised;
-
-                    RemoveUnnecessaryResultMessages(actual, expected);
-
-                    return expected.Equals(actual);
-                }
-
-                if (shouldFail)
-                {
-                    try
-                    {
-                        conforms();
-                    }
-                    catch
-                    {
-                        Console.WriteLine("{0}=True", item);
-                        continue;
-                    }
-
-                    Console.WriteLine("{0}=False", item);
-                }
-                else
-                {
-                    Console.WriteLine("{0}={1}", item, conforms());
-                }
-            }
-        }
-
-        private static void RemoveUnnecessaryResultMessages(IGraph resultReport, IGraph testReport)
-        {
-            foreach (var t in resultReport.GetTriplesWithPredicate(Vocabulary.ResultMessage).ToList())
-            {
-                if (!testReport.GetTriplesWithPredicateObject(Vocabulary.ResultMessage, t.Object).Any())
-                {
-                    resultReport.Retract(t);
-                }
-            }
+            ImplementationReport.Generate().SaveToFile(@"..\..\..\..\dotnetrdf-shacl-earl.ttl", new CompressingTurtleWriter());
         }
     }
 }
